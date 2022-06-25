c7e6fa3c0cdff957f0b1f12280b0d7a9d6721f66
marouenebg <marwenbg123@gmail.com>
2022-06-25 15:18:46 +0100

    initiating the project + stating with the contract

---
+++ hardhat/contracts/WavePortal.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.4;
+
+//to be able to debug smart contract easily
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("Yo yo, I am a contract and I am smart, hihi");
+    }
+}
